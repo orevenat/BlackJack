890c6ab99dc2360c02f83983ccf9b1cf67855874
orevenat <agrry@yandex.ru>
2019-02-12 00:05:52 +0500

    small value added

--- lib/card.rb
+++ lib/card.rb
@@ -1,10 +1,10 @@
 class Card
   attr_reader :name, :value, :small_value
 
-  def initialize(rank, type, value, small_value = false)
+  def initialize(rank, type, value)
     @name = "#{rank}#{type}"
     @value = value
-    @small_value = small_value if small_value != false
+    @small_value = 1 if value == 11
   end
 
   private
